c72427a5caba5ed485fd3f17ce083c9ae3e7f60b
Peter Mansour <pmansour112@gmail.com>
2019-08-04 13:12:11 -0700

    Created simple node.js application

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+import express from 'express';
+
+/** Default port used to start web application locally. */
+const DEFAULT_PORT = 8080;
+
+const app = express();
+app.use(express.static('public'));
+
+const port = process.env.PORT || DEFAULT_PORT;
+app.listen(port);
+
+console.log('Started listening on http://localhost:' + port);
